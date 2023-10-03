14cfd8a3f0342b8bb5ae7c372015032a80a5d46c
Vladislav Kartashov <kartashovladislav@gmail.com>
2023-10-03 23:00:00 +0300

    MVC: Post, User entities

--- spring-mvc/src/main/java/software/darkmatter/school/blog/domain/user/data/User.java
+++ spring-mvc/src/main/java/software/darkmatter/school/blog/domain/user/data/User.java
@@ -1,37 +1,28 @@
-package software.darkmatter.school.blog.domain.post.data;
+package software.darkmatter.school.blog.domain.user.data;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
-import software.darkmatter.school.blog.domain.user.data.User;
 
 import java.time.OffsetDateTime;
 
 @Data
 @Entity
-@Table(name = "posts")
-public class Post {
+@Table(name = "users")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private User user;
+    private String firstName;
 
-    private String title;
-
-    private String summary;
-
-    private String content;
+    private String lastName;
 
     private OffsetDateTime createdAt;
 
     private OffsetDateTime updatedAt;
-
-    private OffsetDateTime publishedAt;
 }
