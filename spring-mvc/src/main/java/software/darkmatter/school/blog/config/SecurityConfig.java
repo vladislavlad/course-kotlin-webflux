f0165fabc99ac90b1afa2e242947aa1842b8a54c
Vladislav Kartashov <kartashovladislav@gmail.com>
2023-10-08 23:46:49 +0300

    MVC: Security config improved, Spring Boot 3.1.4

--- spring-mvc/src/main/java/software/darkmatter/school/blog/config/SecurityConfig.java
+++ spring-mvc/src/main/java/software/darkmatter/school/blog/config/SecurityConfig.java
@@ -1,11 +1,38 @@
 package software.darkmatter.school.blog.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationConverter authenticationConverter;
 
+    private final AuthenticationManager authenticationManager;
+
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+        final AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, authenticationConverter);
+
+        http
+            .addFilter(authenticationFilter)
+            .authorizeHttpRequests(
+                requestMatcherRegistry -> requestMatcherRegistry.anyRequest().authenticated()
+            )
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 }
